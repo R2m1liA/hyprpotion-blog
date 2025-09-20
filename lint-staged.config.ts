31719d21718a80c937f212e0fcca208ff785013d
R2m1liA <15258427350@163.com>
2025-09-20 17:19:23 +0800

    chore: 添加开发依赖
    - 添加lint-staged用于格式化代码

---
+++ lint-staged.config.ts
@@ -0,0 +1,7 @@
+/**
+ * @see https://github.com/lint-staged/lint-staged?tab=readme-ov-file#configuration
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  '*': 'prettier --write',
+};
