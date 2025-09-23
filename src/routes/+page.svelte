83e11a0fe42e3295ed1c9b8bdb7c6756a29a1f97
R2m1liA <15258427350@163.com>
2025-09-23 17:34:22 +0800

    docs(blog): 添加新的文章
    - 关于tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,15 @@
-<div class="container">
-  <h1>Blog with svelte</h1>
-  <p>这是一个使用Svelte构建的博客网站，这是他现在的样子</p>
+<div class="m-[10%_auto_0] w-[60%] bg-gray-200 p-[2%_5%] rounded-[10px]">
+  <h1 class="text-3xl font-bold">Blog with svelte</h1>
+  <p>这是一个使用Svelte构建的博客网站，这是他一开始的样子</p>
+  <img src="/images/initial-page.png" alt="Initial Page Screenshot" />
+  <p>当前为其添加了Tailwindcss</p>
   <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
+    Visit <a class="text-[#d3390a]" href="https://svelte.dev/docs/kit"
+      >svelte.dev/docs/kit</a
+    > to read the documentation
   </p>
 </div>
 
-<style>
-    .container {
-        width: 60%;
-        margin: 10% auto 0;
-        background-color: #f0f0f0;
-        padding: 2% 5%;
-        border-radius: 10px;
-
-        a {
-            color: #D3390A;
-        }
-    }
+<style lang="postcss">
+  @reference "tailwindcss";
 </style>
---
+++ src/routes/initial-page/+page.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+  <h1>Blog with svelte</h1>
+  <p>这是一个使用Svelte构建的博客网站，这是他现在的样子</p>
+  <p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+  </p>
+</div>
+
+<style>
+  .container {
+    width: 60%;
+    margin: 10% auto 0;
+    background-color: #f0f0f0;
+    padding: 2% 5%;
+    border-radius: 10px;
+
+    a {
+      color: #d3390a;
+    }
+  }
+</style>
