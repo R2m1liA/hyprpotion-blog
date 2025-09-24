f454bb5b85f7223628648f378e5cb1c5075ba8cf
R2m1liA <15258427350@163.com>
2025-09-24 22:18:02 +0800

    chore: 添加项目依赖
    - 添加mdsvex用于markdown渲染
    - 为项目配置mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+  extensions: ['.md'],
+  // mdsvex options
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', '.md'],
+
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
